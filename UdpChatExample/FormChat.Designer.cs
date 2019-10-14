34818a6e0a2d20985bcdb21bf18a885d9272c2f5
wnmwi <383929371@qq.com>
2019-10-14 22:20:40 +0800

    第一次提交

---
+++ UdpChatExample/FormChat.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace UdpChatExample
+{
+    partial class FormChat
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.listBoxReceive = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxRemoteIP = new System.Windows.Forms.TextBox();
+            this.textBoxSend = new System.Windows.Forms.TextBox();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listBoxReceive);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(460, 278);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "接收信息";
+            // 
+            // listBoxReceive
+            // 
+            this.listBoxReceive.FormattingEnabled = true;
+            this.listBoxReceive.ItemHeight = 17;
+            this.listBoxReceive.Location = new System.Drawing.Point(6, 22);
+            this.listBoxReceive.Name = "listBoxReceive";
+            this.listBoxReceive.Size = new System.Drawing.Size(448, 242);
+            this.listBoxReceive.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 299);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "远程IP：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 328);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "发送信息：";
+            // 
+            // textBoxRemoteIP
+            // 
+            this.textBoxRemoteIP.Location = new System.Drawing.Point(86, 296);
+            this.textBoxRemoteIP.Name = "textBoxRemoteIP";
+            this.textBoxRemoteIP.Size = new System.Drawing.Size(305, 23);
+            this.textBoxRemoteIP.TabIndex = 3;
+            // 
+            // textBoxSend
+            // 
+            this.textBoxSend.Location = new System.Drawing.Point(86, 325);
+            this.textBoxSend.Name = "textBoxSend";
+            this.textBoxSend.Size = new System.Drawing.Size(305, 23);
+            this.textBoxSend.TabIndex = 4;
+            this.textBoxSend.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxSend_KeyPress);
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Location = new System.Drawing.Point(397, 324);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(75, 25);
+            this.buttonSend.TabIndex = 5;
+            this.buttonSend.Text = "发送";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // FormChat
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 361);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.textBoxSend);
+            this.Controls.Add(this.textBoxRemoteIP);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "FormChat";
+            this.Text = "Udp网络聊天";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormChat_FormClosing);
+            this.Load += new System.EventHandler(this.FormChat_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox listBoxReceive;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxRemoteIP;
+        private System.Windows.Forms.TextBox textBoxSend;
+        private System.Windows.Forms.Button buttonSend;
+    }
+}
+
---
+++ UdpChatExample/FormChat.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace UdpChatExample
+{
+    public partial class FormChat : Form
+    {
+        delegate void AddListBoxItemCallback(string text);
+        AddListBoxItemCallback listBoxCallback;
+        //使用接收端口号
+        private int port = 8001;
+        private UdpClient udpClient;
+        public FormChat()
+        {
+            InitializeComponent();
+            listBoxCallback = new AddListBoxItemCallback(AddListBoxItem);
+        }
+        private void AddListBoxItem(string text)
+        {
+            //如果listBoxReceive被不同的线程访问则通过委托处理
+            if(listBoxReceive.InvokeRequired)
+            {
+                this.Invoke(listBoxCallback, text);
+            }
+            else
+            {
+                listBoxReceive.Items.Add(text);
+                listBoxReceive.SelectedIndex = listBoxReceive.Items.Count - 1;
+            }
+        }
+
+        /// <summary>
+        /// 在后台运行的接收线程
+        /// </summary>
+        private void ReceiveData()
+        {
+            IPAddress[] localIPs = Dns.GetHostAddresses(Dns.GetHostName());
+            //在本机的指定的端口接收
+            //udpClient = new UdpClient(port); //此方式不兼容 ipv6
+            udpClient = new UdpClient(port, localIPs[0].AddressFamily); //此方式兼容 ipv6
+            IPEndPoint remote = null;
+            //接收从远程主机发送过来的信息
+            while(true)
+            {
+                try
+                {
+                    //关闭 udpClient 时此句会产生异常
+                    byte[] bytes = udpClient.Receive(ref remote);
+                    string str = Encoding.UTF8.GetString(bytes, 0, bytes.Length);
+                    AddListBoxItem(string.Format("来自{0}：{1}", remote, str));
+                }
+                catch
+                {
+                    //退出循环，结束线程
+                    break;
+                }
+            }
+        }
+
+        private void SendData()
+        {
+            IPAddress remoteIP;
+            if(IPAddress.TryParse(textBoxRemoteIP.Text,out remoteIP)==false)
+            {
+                MessageBox.Show("远程IP格式不正确");
+                return;
+            }
+            IPEndPoint iep = new IPEndPoint(remoteIP, port);
+            byte[] bytes = Encoding.UTF8.GetBytes(textBoxSend.Text);
+            UdpClient myUdpClient = new UdpClient(remoteIP.AddressFamily); //此方式兼容 ipv6
+            try
+            {
+                myUdpClient.Send(bytes, bytes.Length, iep);
+                textBoxSend.Clear();
+                myUdpClient.Close();
+                textBoxSend.Focus();
+            }
+            catch(Exception err)
+            {
+                MessageBox.Show(err.Message, "发送失败");
+            }
+            finally
+            {
+                myUdpClient.Close();
+            }
+        }
+
+        private void FormChat_Load(object sender, EventArgs e)
+        {
+            //设置listBox 样式
+            listBoxReceive.HorizontalScrollbar = true;
+            listBoxReceive.Dock = DockStyle.Fill;
+            //获取本机第一个可用IP 地址
+            IPAddress myIP = (IPAddress)Dns.GetHostAddresses(Dns.GetHostName()).GetValue(0);
+            //为了在同一台机器调试，此IP 也作为默认远程IP
+            textBoxRemoteIP.Text = myIP.ToString();
+            //创建一个线程接收远程主机发来的信息
+            Thread myThread = new Thread(new ThreadStart(ReceiveData));
+            //将线程设为后台运行
+            myThread.IsBackground = true;
+            myThread.Start();
+            textBoxSend.Focus();
+        }
+
+        /// <summary>
+        /// 单击发送按钮触发的事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            SendData();
+        }
+
+        /// <summary>
+        /// 在textBoxSend 中按下并释放Enter 键后触发的事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void textBoxSend_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+                SendData();
+        }
+
+        private void FormChat_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            udpClient.Close();
+        }
+    }
+}
